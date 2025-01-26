e0fc7c4d04cba712c991a06e863875aadfce4aa6
Jesse Zhou <jessezhou@Jesses-MacBook-Pro.local>
2025-01-26 17:45:07 +0800

    gshh3. FOV=180 degrees, tdod=22000, q=8, qp = 1, m = 100. Gaseous phase

--- program.jl
+++ program.jl
@@ -7,7 +7,7 @@ include("prog.h")
 adata = [(normalised_true_area, StatsBase.mean)]
 mdata = [mean_speed, rot_o_alt, rot_o, polarisation]
 
-target_dods = [1*sqrt(12)]
+target_dods = [22000.0]
 q_args = [8]
 qp_args = [1]
 m_args = [100]
@@ -23,8 +23,8 @@ parameters = Dict(
         #:left_bias_arg => left_biases
 ) 
 
-simulation_marker = :circle
-simulation_markersize = 12
+simulation_marker = arrow_marker
+simulation_markersize = 4
 
 
 
